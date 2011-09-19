646092c0c0381baa8e6968153ff963dfde27419a
Sangwook Kim <swkim@csl.skku.edu>
2011-09-19 23:09:06 +0900

    minor revision

--- skbench/control.py
+++ skbench/control.py
@@ -5,107 +5,35 @@ import time
 from config import *
 
 class Control:
-	def __init__(self):
-		if not os.path.exists(mount_point):
-			os.makedirs(mount_point)
-
-	def __del__(self):
-		for guest in os.listdir(mount_point):
-			self.umount(guest)
-
 	def list(self):
 		return os.listdir(guests_img_root)
 
-	def mount_target(self, guest):
-		return '%s/%s' % (mount_point, guest)
-
-	def mount_ubuntu(self, target):
-		ret = os.system('mount /dev/nbd0p1 %s' % target)
-		return ret
-
-	def mount_fedora(self, target):
-		ret = os.system('vgscan')
-		assert ret == 0
-		ret = os.system('vgchange -ay');
-		assert ret == 0
-		ret = os.system('mount /dev/VolGroup/lv_root %s' % target)
-		return ret
-
-	def mount_windows(self, target):
-		ret = os.system('mount /dev/nbd0p2 %s' % target)
-		return ret
-
-	def mount(self, guest):
-		image_path = rootfs_map[guest]
-		print 'rootfs path: %s' % image_path 
-		target = self.mount_target(guest)
-
-		os.mkdir(target)
-		ret = os.system('modprobe nbd max_part=8')
-		assert ret == 0
-		ret = os.system('qemu-nbd -c /dev/nbd0 %s' % image_path)
-		assert ret == 0
-		# TODO: automatic primary partition discovery
-		time.sleep(3)
-		
-		if guest in active_ubuntu_guests:
-			ret = self.mount_ubuntu(target)
-		if guest in active_fedora_guests:
-			ret = self.mount_fedora(target)
-		if guest in active_windows_guests:
-			ret = self.mount_windows(target)
-		assert ret == 0
-		
-		return target
-
-	def umount(self, guest):
-		target = self.mount_target(guest)
-		if not os.path.exists(target):
-			return
-
-		ret = os.system('umount %s' % target)
-		assert ret == 0
-		os.rmdir(target)
-		
-		if guest in active_fedora_guests:
-			ret = os.system('vgchange -an VolGroup')
-			assert ret == 0
-
-		ret = os.system('qemu-nbd -d /dev/nbd0')
-		assert ret == 0
-
-	def create(self, guest, blocking=True):
-		self._execute_cmd('virsh create %s' % config_map[guest], blocking)
-
-	def shutdown(self, guest, blocking=True):
-		if blocking:
-			self._execute_cmd('virsh shutdown %s' % guest, True)
-		else:
-			self._execute_cmd('virsh shutdown %s' % guest, False)
-
-	def shutdown_all(self, blocking=True):
-		if blocking:
-			for	guest in active_linux_guests:
-				self._execute_cmd('virsh shutdown %s' % guest, True)
-		else:
-			for guest in active_linux_guests:
-				self._execute_cmd('virsh shutdown %s' % guest, False)
-
-		if blocking:
-			for	guest in active_windows_guests:
-				self._execute_cmd('virsh destroy %s' % guest, True)
-		else:
-			for guest in active_windows_guests:
-				self._execute_cmd('virsh destroy %s' % guest, False)
-
 	def destroy(self, guest, blocking=True):
 		self._execute_cmd('virsh destroy %s' % guest, blocking)
+		img_path = guests_image_map[guest]
+		backup_path = img_path + '.bak'
+		ret = os.system('rm %s' % img_path)
+		assert ret == 0
+		ret = os.system('mv %s %s' % (backup_path, img_path))
+		assert ret == 0
 
 	def destroy_all(self, blocking=True):
-		for guest in active_guests:
+		for guest in saved_guests:
 			self._execute_cmd('virsh destroy %s' % guest, blocking)
+			img_path = guests_image_map[guest]
+			backup_path = img_path + '.bak'
+			ret = os.system('rm %s' % img_path)
+			assert ret == 0
+			ret = os.system('mv %s %s' % (backup_path, img_path))
+			assert ret == 0
 
 	def restore(self, guest):
+		img_path = guests_image_map[guest]
+		backup_path = img_path + '.bak'
+		ret = os.system('mv %s %s' % (img_path, backup_path))
+		assert ret == 0
+		ret = os.system('qemu-img create -f qcow2 -b %s %s' % (backup_path, img_path))
+		assert ret == 0
 		self._execute_cmd('virsh restore %s' % guests_saved_state_map[guest], True)
 
 	def _execute_cmd(self, cmd, blocking):
--- skbench/gen_script.py
+++ skbench/gen_script.py
@@ -6,22 +6,12 @@ default_disabled_services = ['sysklogd', 'klogd']
 jobs = []
 
 class Gen_script:
-	def __init__(self, workload_file, private_arg, active_guest_num, auto_mode, passive_mode):
+	def __init__(self, workload_file, private_arg):
         # load config file and workload file
 		exec( open( workload_file ).read() )
 
-		num_of_guests = active_guest_num;
-		if num_of_guests == 0:
-			num_of_guests = len( workload_scripts )
-
-		wait_guest_num = 1
-		if auto_mode == 1:
-			wait_guest_num = num_of_guests
-		
-		self.disabled_services = {}
-		for n in range(num_of_guests):
-			self.disabled_services[1 + n] = default_disabled_services
-
+		num_of_guests = len(active_guests)
+	
 		self.job = {}
 
         ####### Host's job is treated separately #######
@@ -31,37 +21,22 @@ class Gen_script:
 				nc -l -p $((20000 + $N)) > /tmp/result$N &
 			done
 		"""
-		#TODO: multiple vcpu handling
-		#self.job[0] += """
-		#pcpu_number=`cat /proc/cpuinfo | grep "siblings" | tail -1 | cut -d: -f2`
-		#"""
-		pcpu_num = 0
-		for guest in active_guests:
-			self.job[0] += "virsh vcpupin %s 0 %d\n" % (guest, pcpu_num)
-			self.job[0] += "virsh vcpuinfo %s\n" % guest
-			pcpu_num += 1
-			pcpu_num %= 4
+		
+		self.job[0] += """
+			for GUEST_IP in $GUEST_IPS; do
+				ssh $GUEST_IP ~/job > /tmp/log &
+			done
+		"""
 
 		self.job[0] += """
-			# wait for guests' booting
+			# wait for guests' initializing
 			wait_signal_64 %d 10000
 
 			virsh list
-			echo "[All guests finish booting]"
+			echo "[All guests are ready to execute]"
 		""" % num_of_guests
 
 		self.job[0] += "PRIVATE_ARG=%s          # for prolog and epilog\n" % private_arg
-
-		#TODO: clarify manual mode
-		if auto_mode == 0:
-			self.job[0] += """
-				echo "[Manual test mode]" 
-				echo "  To start, send_signal $IP_HOST 10001"
-				echo "  To end,   send_signal $IP_HOST 10002"
-				sleep 3; 
-				echo $PRIVATE_ARG | nc canh1.kaist.ac.kr 33333          # semi-auto 
-				wait_signal_64 1 10001
-			""" 
 
         # Prolog part
 		self.job[0] += "# prolog\n"
@@ -91,17 +66,7 @@ class Gen_script:
 			echo "[Workloads end]"
 
 			# epilog 
-		""" % len(active_guests) #wait_guest_num
-
-#TODO: adjust when auto_mode == 0
-		if auto_mode == 0:
-			self.job[0] += """
-				# send signals to all guest with WAIT_STOP_SIGNAL
-				echo "Sending stop signals to all domU with WAIT_STOP_SIGNAL"
-				for DOMU_IP in $DOMU_IPS; do
-					send_signal_64 $DOMU_IP 10002
-				done
-			"""
+		""" % num_of_guests
 
 		f = open( 'scripts/' + epilog_script )
 		lines = f.readlines()
@@ -119,51 +84,30 @@ class Gen_script:
 		"""
 
         ####### Generate scripts for guest based on workload file #######
-		for guest_id in range(len(active_linux_guests)):
+		for guest_id in range(len(active_guests)):
 			job_id = guest_id + 1;     
-
+			
 			self.job[job_id] = 'export PRIVATE_ARG=%s           # private argument provided by skbench\n' % private_arg
-			if passive_mode:
-				self.job[job_id] += 'export SKBENCH=1                # indicate following workload is started by skbench\n'
-				self.job[job_id] += 'export PATH=$PATH:/usr/local/bin\n'
 
 			f = open( 'scripts/' + workload_scripts[guest_id] )
 			self.job[job_id] += f.read()
 			f.close()
 
 			# replace predefined macros with real commands
-			if passive_mode == 0:
-                # replace WAIT_START_SIGNAL with real command. If this macro is not found, real command is augmented at start.
-				self.job[job_id], nr_rep = re.subn( "WAIT_START_SIGNAL", "wait_signal_32 1 10001   # WAIT_START_SIGNAL\n", self.job[job_id] )
-				if nr_rep < 1:
-					self.job[job_id] = 'wait_signal_32 1 10001    # WAIT_START_SIGNAL\n' + self.job[job_id]
+			guest_name = active_guests[guest_id]
+            # replace WAIT_START_SIGNAL with real command. If this macro is not found, real command is augmented at start.
+			self.job[job_id], nr_rep = re.subn( "WAIT_START_SIGNAL", "wait_signal_%s 1 10001   # WAIT_START_SIGNAL\n" % guest_bitness[guest_name], self.job[job_id] )
 
-                # replace SEND_BOOT_SIGNAL with real command. If this macro is not found, real command is augmented at start.
-				self.job[job_id], nr_rep = re.subn( "SEND_BOOT_SIGNAL", "send_signal_32 $IP_HOST 10000     # SEND_BOOT_SIGNAL\n", self.job[job_id] )
-				if nr_rep < 1:
-					self.job[job_id] = 'send_signal_32 $IP_HOST 10000   # SEND_BOOT_SIGNAL\n' + self.job[job_id]
+            # replace SEND_BOOT_SIGNAL with real command. If this macro is not found, real command is augmented at start.
+			self.job[job_id], nr_rep = re.subn( "SEND_READY_SIGNAL", "send_signal_%s $IP_HOST 10000     # SEND_BOOT_SIGNAL\n" % guest_bitness[guest_name], self.job[job_id] )
+			if nr_rep < 1:
+				self.job[job_id] = 'send_signal_%s $IP_HOST 10000   # SEND_BOOT_SIGNAL\n' % guest_bitness[guest_name] + self.job[job_id]
 
-				if auto_mode == 0:
-                    # replace WAIT_STOP_SIGNAL with real command. If this macro is not found, real command is augmented at end.
-					self.job[job_id], nr_rep = re.subn( "WAIT_STOP_SIGNAL", "wait_signal_32 1 10002    # WAIT_STOP_SIGNAL", self.job[job_id] )
-					if nr_rep < 1:
-						self.job[job_id] += 'wait_signal_32 1 10002    # WAIT_STOP_SIGNAL\n'
-
-			if active_linux_guests[guest_id] in active_ubuntu_guests:
-				self.job[job_id] = re.sub( "TO_HOST", "nc -q 0 $IP_HOST $((20000 + %d))" % job_id, self.job[job_id] )
-			else:
-				self.job[job_id] = re.sub( "TO_HOST", "nc $IP_HOST $((20000 + %d))" % job_id, self.job[job_id] )
+#			if active_linux_guests[guest_id] in active_ubuntu_guests:
+			self.job[job_id] = re.sub( "TO_HOST", "nc -q 0 $IP_HOST $((20000 + %d))" % job_id, self.job[job_id] )
+#			else:
+#				self.job[job_id] = re.sub( "TO_HOST", "nc $IP_HOST $((20000 + %d))" % job_id, self.job[job_id] )
 	
             # Workload start
 
-			if auto_mode == 1:
-				self.job[job_id] += 'send_signal_32 $IP_HOST 10002\n'
-
-		for guest_id in range(len(active_windows_guests)):
-			guest_id += len(active_linux_guests);
-			job_id = guest_id + 1;
-			self.job[job_id] = ''
-			f = open( 'scripts/' + workload_scripts[guest_id] )
-			self.job[job_id] += f.read()
-			f.close()
-			
+			self.job[job_id] += 'send_signal_%s $IP_HOST 10002\n' % guest_bitness[guest_name]			
