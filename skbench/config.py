83c89c491e2e5512a37f61cf7517fdf967c6d487
root <root@Xeon-server.(none)>
2011-10-07 19:49:48 +0900

    sample scripts are added

--- skbench/config.py
+++ skbench/config.py
@@ -6,24 +6,24 @@ import os
 import sys
 
 guests_saved_state_map = {
-	'parsec1': '/mnt/sdb/vm_saved/parsec1.saved',
-	'parsec2': '/mnt/sdb/vm_saved/parsec2.saved',
-	'parsec3': '/mnt/sdb/vm_saved/parsec3.saved',
-	'parsec4': '/mnt/sdb/vm_saved/parsec4.saved',
-	'interactive': '/mnt/sdb/vm_saved/interactive.saved',
+	'parsec1': '/root/vm_saved/parsec1.saved',
+	'parsec2': '/root/vm_saved/parsec2.saved',
+	'parsec3': '/root/vm_saved/parsec3.saved',
+	'parsec4': '/root/vm_saved/parsec4.saved',
+#'interactive': '/root/vm_saved/interactive.saved',
 }
 
 guests_image_map = {
-	'parsec1': '/mnt/sdb/vm_images/parsec1.qcow2',
-	'parsec2': '/mnt/sdb/vm_images/parsec2.qcow2',
-	'parsec3': '/mnt/sdb/vm_images/parsec3.qcow2',
-	'parsec4': '/mnt/sdb/vm_images/parsec4.qcow2',
-	'interactive': '/mnt/sdb/vm_images/interactive.qcow2',
+	'parsec1': '/root/vm_images/parsec1.qcow2',
+	'parsec2': '/root/vm_images/parsec2.qcow2',
+	'parsec3': '/root/vm_images/parsec3.qcow2',
+	'parsec4': '/root/vm_images/parsec4.qcow2',
+#'interactive': '/root/vm_images/interactive.qcow2',
 }
 
-host_ip = '115.145.212.176'
+host_ip = '115.145.212.186'
 
-trace_replay = 1
+trace_replay = 0
 
 client_machine_ip = '115.145.212.176'
 
@@ -45,6 +45,6 @@ guest_bitness = {
 
 active_guests = ['parsec1', 'parsec2', 'parsec3', 'parsec4']   # active guest's names who participate in the benchmark
 
-trace_guests = ['interactive']
+trace_guests = ['']
 
 dist = [os.path.normpath(sys.modules[__name__].__file__ + '/../dist')]
