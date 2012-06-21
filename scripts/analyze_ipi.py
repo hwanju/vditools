be59fd73387faa6f35f2abc5fec87f8589203bfb
Hwanju Kim <hwandori@gmail.com>
2012-06-22 07:32:14 +0900

    trace ipi scripts

--- scripts/analyze_ipi.py
+++ scripts/analyze_ipi.py
@@ -3,22 +3,55 @@
 import sys
 import re
 
-FlowID = 0
-class CascadedIPIFlow:
-	def __init__(self, src_vcpu_id, dst_vcpu_id):
-		global FlowID
-		self.flow_id = FlowID 
-		FlowID = FlowID + 1
-		self.vcpu_seq = [src_vcpu_id, dst_vcpu_id]
-	def extend_flow(self, src_vcpu_id, dst_vcpu_id):
-		if src_vcpu_id == self.vcpu_seq[-1]:	# linked?
-			if dst_vcpu_id in self.vcpu_seq:	# not disjoint
-				return -1
-			self.vcpu_seq.append(dst_vcpu_id)	# cascased (linked & disjoint)
-			return 1
-		return 0
-	def cmp_flow(self, vcpu_id):
-		return vcpu_id == self.vcpu_seq[-1]
+class ReschedIpiFlow:
+	def __init__(self):
+		self.vcpu_flow = []
+		self.last_dst_vcpus = []
+		self.flow_type = 1
+		self.start_time_us = 0
+
+	def update_flow(self, time_us, runtime_after_sched_us, src_vcpu_id, dst_vcpu_id):
+		if len(self.vcpu_flow) == 0:
+			self.vcpu_flow.append((src_vcpu_id, dst_vcpu_id))
+			self.last_dst_vcpus.append(dst_vcpu_id)
+			return
+
+		# previous flow exists
+		if runtime_after_sched_us <= 100:	# flow threshold
+			cascaded = src_vcpu_id in self.last_dst_vcpus
+			if cascaded:
+				if self.flow_type == 2 and src_vcpu_id == self.last_dst_vcpus[0]:
+					self.flow_type = 3
+				self.last_dst_vcpus.remove(src_vcpu_id)
+				self.chunk_flow(time_us, False)
+			elif src_vcpu_id == self.vcpu_flow[-1][0]:	# type 2 or 3 flow detected	
+				if len(self.vcpu_flow) > 1:
+					self.chunk_flow(time_us, True, True)	# chunk previous flow
+				self.last_dst_vcpus = [self.vcpu_flow[0][1]]
+				self.flow_type = 2
+			else:
+				self.chunk_flow(time_us, True)
+		else:
+			self.chunk_flow(time_us, True)
+
+		self.vcpu_flow.append((src_vcpu_id, dst_vcpu_id))
+		self.last_dst_vcpus.append(dst_vcpu_id)
+
+	def chunk_flow(self, time_us, force, keep_tail=False):
+		if force or (self.flow_type == 2 and len(self.vcpu_flow) == 9) or (self.flow_type != 2 and len(self.vcpu_flow) == 8): 
+			print "%d - %d: flow t=%d n=%d" % (self.start_time_us, time_us, self.flow_type, len(self.vcpu_flow)), self.vcpu_flow
+			if keep_tail:
+				self.vcpu_flow = self.vcpu_flow[-1:]
+			else:
+				self.vcpu_flow = []
+
+			self.flow_type = 1
+			self.start_time_us = time_us
+	def update_terminal(self, time_us, vcpu_id):
+		if vcpu_id in self.last_dst_vcpus:
+			self.last_dst_vcpus.remove(vcpu_id)
+			if len(self.last_dst_vcpus) == 0:
+				self.chunk_flow(time_us, True)
 
 if (len(sys.argv) != 2):
         print "Usage: %s <trace file>" % sys.argv[0]
@@ -26,40 +59,24 @@ if (len(sys.argv) != 2):
 trace_fn = sys.argv[1];
 trace_file = open(trace_fn, 'r')
 
-ipi_event = re.compile(r'''([0-9]+) I fd ([0-9]+) ([0-9]+)''')
+ipi_event = re.compile(r'''([0-9]+) ([0-9]+) I fd ([0-9]+) ([0-9]+)''')
 blk_event = re.compile(r'''([0-9]+) B ([0-9]+)''')
 
-cascaded_ipi_flow_list = []
-
+resched_ipi_flow = ReschedIpiFlow()
 for line in trace_file:
 	p = ipi_event.search(line)
 	if not (p == None):
 		time_us = int(p.group(1))
-		src_vcpu_id = int(p.group(2))
-		dst_vcpu_id = int(p.group(3))
+		runtime_after_sched_us = int(p.group(2))
+		src_vcpu_id = int(p.group(3))
+		dst_vcpu_id = int(p.group(4))
 
-		new = True
-		for flow in cascaded_ipi_flow_list:
-			ret = flow.extend_flow(src_vcpu_id, dst_vcpu_id)
-			if ret == 1:
-				print "%d ext: f%d %d->%d n=%d" % (time_us, flow.flow_id, src_vcpu_id, dst_vcpu_id, len(flow.vcpu_seq)), flow.vcpu_seq
-				new = False
-			elif ret == -1:
-				if len(flow.vcpu_seq) == 8:
-					print "%d br: f%d %d->%d n=%d" % (time_us, flow.flow_id, src_vcpu_id, dst_vcpu_id, len(flow.vcpu_seq)), flow.vcpu_seq
-				else:
-					print "%d cc: f%d %d->%d n=%d" % (time_us, flow.flow_id, src_vcpu_id, dst_vcpu_id, len(flow.vcpu_seq)), flow.vcpu_seq
-				cascaded_ipi_flow_list.remove(flow)
-		if new:	# not found
-			flow = CascadedIPIFlow(src_vcpu_id, dst_vcpu_id)
-			print "%d new: f%d %d->%d" % (time_us, flow.flow_id, src_vcpu_id, dst_vcpu_id)
-			cascaded_ipi_flow_list.append(flow)
+		resched_ipi_flow.update_flow(time_us, runtime_after_sched_us, src_vcpu_id, dst_vcpu_id)
+
 	p = blk_event.search(line)
 	if not (p == None):
 		time_us = int(p.group(1))
 		vcpu_id = int(p.group(2))
-		for flow in cascaded_ipi_flow_list:
-			if flow.cmp_flow(vcpu_id):
-				print "%d end: f%d %d n=%d" % (time_us, flow.flow_id, vcpu_id, len(flow.vcpu_seq)), flow.vcpu_seq
-				cascaded_ipi_flow_list.remove(flow)
 
+		resched_ipi_flow.update_terminal(time_us, vcpu_id)
+		
