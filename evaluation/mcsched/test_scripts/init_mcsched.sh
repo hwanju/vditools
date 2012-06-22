6b50c2c8469e8d10a1c269ed6efc2eb902c7cd39
Hwanju Kim <hwandori@gmail.com>
2012-06-22 20:20:15 +0900

    simple init_mcsched.sh analyze_ipi.py modified

---
+++ evaluation/mcsched/test_scripts/init_mcsched.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+if [ $# -ne 1 ]; then
+	echo "Usage $0 <0=off|1=on>"
+	exit
+fi
+on=$1
+
+if [ ! -e /cpuctl/g1 ]; then
+	cpuctl=/cpuctl
+	mkdir -p $cpuctl
+	mount -t cgroup -o cpu none $cpuctl
+	for i in `seq 1 10`; do
+		mkdir -p $cpuctl/g$i
+		echo 65536 > $cpuctl/g$i/cpu.shares
+	done
+fi
+if [ $on -eq 0 ]; then
+	echo 0 > /proc/sys/kernel/sched_urgent_enabled
+	echo 0 > /proc/sys/kernel/sched_urgent_tslice_limit_ns
+	echo 0 > /sys/module/kvm/parameters/resched_ipi_unlock_latency_ns
+	echo 0 > /sys/module/kvm/parameters/resched_ipi_cosched_tslice_ns
+	echo 0 > /sys/module/kvm/parameters/tlb_shootdown_latency_ns
+
+	echo "mcsched off"
+else
+	echo 1 > /proc/sys/kernel/sched_urgent_enabled
+	echo 1000000 > /proc/sys/kernel/sched_urgent_tslice_limit_ns
+	echo 500000 > /sys/module/kvm/parameters/resched_ipi_unlock_latency_ns
+	echo 500000 > /sys/module/kvm/parameters/resched_ipi_cosched_tslice_ns
+	echo 100000 > /sys/module/kvm/parameters/tlb_shootdown_latency_ns
+
+	echo "mcsched on"
+fi
--- scripts/analyze_ipi.py
+++ scripts/analyze_ipi.py
@@ -2,6 +2,7 @@
 
 import sys
 import re
+import copy
 
 class ReschedIpiFlow:
 	def __init__(self):
@@ -10,44 +11,54 @@ class ReschedIpiFlow:
 		self.flow_type = 1
 		self.start_time_us = 0
 
+	def add_entry(self, time_us, src_vcpu_id, dst_vcpu_id):
+		self.vcpu_flow.append([src_vcpu_id, dst_vcpu_id])
+		self.last_dst_vcpus.append(dst_vcpu_id)
+
+	def check_full(self):
+		return (self.flow_type != 2 and len(self.vcpu_flow) == 7) or (self.flow_type == 2 and len(self.vcpu_flow) == 8)	# FIXME
+
 	def update_flow(self, time_us, runtime_after_sched_us, src_vcpu_id, dst_vcpu_id):
 		if len(self.vcpu_flow) == 0:
-			self.vcpu_flow.append((src_vcpu_id, dst_vcpu_id))
-			self.last_dst_vcpus.append(dst_vcpu_id)
+			self.add_entry(time_us, src_vcpu_id, dst_vcpu_id)
 			return
 
 		# previous flow exists
 		if runtime_after_sched_us <= 100:	# flow threshold
 			cascaded = src_vcpu_id in self.last_dst_vcpus
-			if cascaded:
+
+			if self.check_full():
+				self.chunk_flow(time_us)
+			elif cascaded:
 				if self.flow_type == 2 and src_vcpu_id == self.last_dst_vcpus[0]:
-					self.flow_type = 3
+					self.flow_type == 3
 				self.last_dst_vcpus.remove(src_vcpu_id)
-				self.chunk_flow(time_us, False)
 			elif src_vcpu_id == self.vcpu_flow[-1][0]:	# type 2 or 3 flow detected	
 				if len(self.vcpu_flow) > 1:
-					self.chunk_flow(time_us, True, True)	# chunk previous flow
+					self.chunk_flow(time_us, True)	# chunk previous flow
 				self.last_dst_vcpus = [self.vcpu_flow[0][1]]
 				self.flow_type = 2
 			else:
-				self.chunk_flow(time_us, True)
+				self.chunk_flow(time_us)
 		else:
-			self.chunk_flow(time_us, True)
+			self.chunk_flow(time_us)
 
-		self.vcpu_flow.append((src_vcpu_id, dst_vcpu_id))
-		self.last_dst_vcpus.append(dst_vcpu_id)
+		self.add_entry(time_us, src_vcpu_id, dst_vcpu_id)
 
-	def chunk_flow(self, time_us, force, keep_tail=False):
-		if force or (self.flow_type == 2 and len(self.vcpu_flow) == 9) or (self.flow_type != 2 and len(self.vcpu_flow) == 8): 
-			print "%d - %d: flow t=%d n=%d" % (self.start_time_us, time_us, self.flow_type, len(self.vcpu_flow)), self.vcpu_flow
-			if keep_tail:
-				self.vcpu_flow = self.vcpu_flow[-1:]
-			else:
-				self.vcpu_flow = []
+	def chunk_flow(self, time_us, keep_tail=False):
+		if self.check_full():
+			####print "FULL",
+			global trace_start_time_us
+			print "%d\t1" % (self.start_time_us - trace_start_time_us)
+		#### print "%d flow t=%d n=%d" % (self.start_time_us, self.flow_type, len(self.vcpu_flow)), self.vcpu_flow
+		if keep_tail:
+			self.vcpu_flow = self.vcpu_flow[-1:]
+		else:
+			self.vcpu_flow = []
+		self.flow_type = 1
+		self.start_time_us = time_us
 
-			self.flow_type = 1
-			self.start_time_us = time_us
-	def update_terminal(self, time_us, vcpu_id):
+	def update_terminal(self, time_us, vcpu_id, state):
 		if vcpu_id in self.last_dst_vcpus:
 			self.last_dst_vcpus.remove(vcpu_id)
 			if len(self.last_dst_vcpus) == 0:
@@ -60,8 +71,9 @@ trace_fn = sys.argv[1];
 trace_file = open(trace_fn, 'r')
 
 ipi_event = re.compile(r'''([0-9]+) ([0-9]+) I fd ([0-9]+) ([0-9]+)''')
-blk_event = re.compile(r'''([0-9]+) B ([0-9]+)''')
+desched_event = re.compile(r'''([0-9]+) D ([0-9]+) ([0-9]+)''')
 
+trace_start_time_us = None
 resched_ipi_flow = ReschedIpiFlow()
 for line in trace_file:
 	p = ipi_event.search(line)
@@ -71,12 +83,16 @@ for line in trace_file:
 		src_vcpu_id = int(p.group(3))
 		dst_vcpu_id = int(p.group(4))
 
+		if trace_start_time_us == None:
+			trace_start_time_us = time_us
+
 		resched_ipi_flow.update_flow(time_us, runtime_after_sched_us, src_vcpu_id, dst_vcpu_id)
 
-	p = blk_event.search(line)
+	p = desched_event.search(line)
 	if not (p == None):
 		time_us = int(p.group(1))
 		vcpu_id = int(p.group(2))
+		state = int(p.group(3))
 
-		resched_ipi_flow.update_terminal(time_us, vcpu_id)
+		resched_ipi_flow.update_terminal(time_us, vcpu_id, state)
 		
