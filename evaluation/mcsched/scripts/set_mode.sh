0b36a4304bb69b2da79b61e70931c118eaf511c4
Hwanju Kim <hwandori@gmail.com>
2012-06-26 18:11:22 +0900

    add mem_hog in initial part of parsec workloads

--- evaluation/mcsched/scripts/set_mode.sh
+++ evaluation/mcsched/scripts/set_mode.sh
@@ -6,7 +6,8 @@ if [ $# -ne 1 ]; then
 fi
 mode=$1
 
-prolog_list="baseline_prolog purebal_prolog purebal_mig_prolog fairbal_0pct_prolog fairbal_100pct_prolog fairbal_150pct_prolog fairbal_200pct_prolog fairbal_250pct_prolog fairbal_300pct_prolog"
+#prolog_list="baseline_prolog purebal_prolog purebal_mig_prolog fairbal_0pct_prolog fairbal_100pct_prolog fairbal_150pct_prolog fairbal_200pct_prolog fairbal_250pct_prolog fairbal_300pct_prolog"
+prolog_list="baseline_prolog purebal_prolog purebal_mig_prolog fairbal_0pct_prolog fairbal_100pct_prolog"
 for p in $prolog_list; do
 	ln -sf $p.$mode $p
 done
