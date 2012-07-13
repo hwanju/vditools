399cd5b491a6e3d2015a9f7287f97961b105da89
Hwanju Kim <hwandori@gmail.com>
2012-07-13 15:13:46 +0900

    eval script

---
+++ evaluation/mcsched/test_scripts/eval.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+if [ $# -lt 2 ]; then
+	echo "Usage: $0 <mode: nouvf, tlbco, tlbco+reschedpd, tlbco+reschedpd+reschedco> workload ..."
+	exit
+fi
+mode=$1
+shift
+if [ "$mode" == "nouvf" ]; then
+	eval_params="0:0:0:0:0:0"
+elif [ "$mode" == "tlbco" ]; then
+	eval_params="1:500000:18000000:1:0:0"
+elif [ "$mode" == "tlbco+reschedpd" ]; then
+	eval_params="1:500000:18000000:1:500000:0"
+elif [ "$mode" == "tlbco+reschedpd+reschedco" ]; then
+	eval_params="1:500000:18000000:1:500000:500000"
+else
+	echo "mode is invalid!"
+	exit
+fi
+workload_list="1parsec+2x264 1parsec+4x264 1parsec+1streamcluster 1parsec+1freqmine 1parsec+1dedup"
+if [ "$1" != "all" ]; then
+	workload_list="$*"
+fi
+for workload in $workload_list; do
+	seq=0
+	if [ $(echo $workload | grep 2x264) ]; then
+		seq=1
+	elif [ $(echo $workload | grep 4x264) ]; then
+		seq=1
+	fi
+	if [ $seq -eq 0 ]; then
+		echo "params=$eval_params ./test_scripts/mcsched.sh $workload fairbal_pct100" 
+	else
+		echo "mixed=1 params=$eval_params ./test_scripts/mcsched.sh $workload fairbal_pct100" 
+	fi
+done
